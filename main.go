8d98db7a9870b85a239ae132249eb8cba11fa1f6
Raghav2211 <rghvj.22@gmail.com>
2022-10-10 23:49:38 +0530

    run specific basics

--- main.go
+++ main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"basics/basics"
+	"errors"
+	"flag"
+	"log"
+	"strings"
 )
 
 func main() {
-	basics.Variables()
-	basics.Primitives()
-	basics.Arrays()
-	basics.Slices()
+
+	runType := flag.String("runtype", "Variables", "Which basics package you want run")
+	flag.Parse()
+	if strings.EqualFold(*runType, "Variables") {
+		basics.Variables()
+	} else if strings.EqualFold(*runType, "Primitives") {
+		basics.Primitives()
+	} else if strings.EqualFold(*runType, "Arrays") {
+		basics.Arrays()
+	} else if strings.EqualFold(*runType, "Slices") {
+		basics.Slices()
+	} else if strings.EqualFold(*runType, "Maps") {
+		basics.Maps()
+	} else if strings.EqualFold(*runType, "Struct") {
+		basics.Struct()
+	} else {
+		err := errors.New("runtype not found")
+		log.Fatal(err)
+	}
 }
