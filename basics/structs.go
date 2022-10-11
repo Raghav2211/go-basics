0318ee9cea58935a123882aba961db8433265ca7
Raghav2211 <rghvj.22@gmail.com>
2022-10-11 19:38:01 +0530

    update struct basics

--- basics/structs.go
+++ basics/structs.go
@@ -16,7 +16,6 @@ func Struct() {
 	}
 
 	fmt.Println("Author info: ", author)
-	fmt.Println("Author fullName : ", author.firstName+" "+author.lastName)
 
 	anonymousAuthor := struct {
 		id        string
@@ -24,20 +23,22 @@ func Struct() {
 		lastName  string
 	}{"1", "Raghav", "Joshi"}
 	fmt.Println("Anonymous Author info", anonymousAuthor)
-	fmt.Println("Anonymous Author info", anonymousAuthor.firstName+" "+anonymousAuthor.lastName)
 
+	// struct assignment
 	anonymousAuthor2 := anonymousAuthor
-	anonymousAuthor2.firstName = "new"
-	anonymousAuthor2.lastName = "user"
 
-	fmt.Println("Anonymous Author info", anonymousAuthor)
-	fmt.Println("Anonymous2 Author info", anonymousAuthor2)
+	// update struct attribute will not effect all assignment
+	anonymousAuthor2.firstName = "Raghav2"
 
+	fmt.Println("[After update]Anonymous Author info", anonymousAuthor)
+	fmt.Println("[After update]Anonymous2 Author info", anonymousAuthor2)
+
+	// struct assignment using pointer
 	refAnonymousAuthor := &anonymousAuthor
 
-	refAnonymousAuthor.firstName = "Ref"
-	refAnonymousAuthor.lastName = "Change"
-	fmt.Println("Anonymous Author info", anonymousAuthor)
-	fmt.Println("Reference Anonymous Author info", refAnonymousAuthor)
+	// update struct attribute will  effect all assignment
+	refAnonymousAuthor.firstName = "Raghav2"
+	fmt.Println("[After update using pointer assignment]Anonymous Author info", anonymousAuthor)
+	fmt.Println("[After update using pointer assignment]Reference Anonymous Author info", refAnonymousAuthor)
 
 }
